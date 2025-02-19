c6e55abe39591fd32215e70bc5de3cd2e3c12761
Ajay <112794893+Ajay-308@users.noreply.github.com>
2025-02-19 11:47:23 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+import requests
+import datetime
+
+def get_leetcode_daily():
+    url = "https://leetcode.com/graphql"
+    headers = {
+        "Content-Type": "application/json"
+    }
+    query = {
+        "query": """
+        query questionOfToday {
+          activeDailyCodingChallengeQuestion {
+            date
+            link
+            question {
+              title
+            }
+          }
+        }
+        """
+    }
+    
+    response = requests.post(url, json=query, headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+        question = data["data"]["activeDailyCodingChallengeQuestion"]
+        date = question["date"]
+        problem_title = question["question"]["title"]
+        problem_link = f"https://leetcode.com{question['link']}"
+        
+        return f"Today's ({date}) LeetCode problem: {problem_title}\n{problem_link}"
+    else:
+        return "Could not fetch today's problem. API error."
+
+print(get_leetcode_daily())
